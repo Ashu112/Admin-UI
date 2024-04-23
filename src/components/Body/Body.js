9fb26d91e129c843de04a0748098f5d0c842b343
Ashu112 <kashu2611@gmail.com>
2024-04-23 17:14:02 +0530

    data fetched

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,7 +1,31 @@
 import React from "react";
+import Search from "../Search/Search";
+import axios from "axios";
+import { useEffect } from "react";
 
 const Body = () => {
-  return <div></div>;
+  const fetchData = async () => {
+    try {
+      const url =
+        "https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json";
+      const response = await axios.get(url);
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  return (
+    <div>
+      <div className="search-continer">
+        <Search />
+      </div>
+      <div className="table-container"></div>
+    </div>
+  );
 };
 
 export default Body;
