156788350c2b6ffe16103301545745a0cc8eedc5
Ashu112 <kashu2611@gmail.com>
2024-04-30 12:15:31 +0530

    Table loaded again

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -11,6 +11,7 @@ import {
   TableCell,
   TableBody,
   Checkbox,
+  Paper,
 } from "@mui/material";
 
 const Body = () => {
@@ -39,25 +40,105 @@ const Body = () => {
       </div>
       <div className="table-container">
         <TableContainer
+          component={Paper}
           style={{
             border: "none",
             backgroundColor: "aqua",
           }}
         >
-          <Table>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
             <TableHead>
               <TableRow>
                 <TableCell align="center">
                   <Checkbox />
                 </TableCell>
-                <TableCell>Name</TableCell>
-                <TableCell>Email</TableCell>
-                <TableCell>Role</TableCell>
-                <TableCell>Action</TableCell>
+                <TableCell
+                  align="center"
+                  style={{
+                    fontWeight: "bold",
+                    fontSize: "1rem",
+                  }}
+                >
+                  Name
+                </TableCell>
+                <TableCell
+                  align="center"
+                  style={{
+                    fontWeight: "bold",
+                    fontSize: "1rem",
+                  }}
+                >
+                  Email
+                </TableCell>
+                <TableCell
+                  align="center"
+                  style={{
+                    fontWeight: "bold",
+                    fontSize: "1rem",
+                  }}
+                >
+                  Role
+                </TableCell>
+                <TableCell
+                  align="center"
+                  style={{
+                    fontWeight: "bold",
+                    fontSize: "1rem",
+                  }}
+                >
+                  Action Items
+                </TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
-              <DataTable dataList={dataList} />
+              {dataList.map((row) => (
+                <TableRow
+                  key={row.name}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  style={{ backgroundColor: "white" }}
+                >
+                  <TableCell
+                    component="th"
+                    scope="row"
+                    align="center"
+                    sx={{ border: "none" }}
+                  >
+                    <Checkbox />
+                  </TableCell>
+                  <TableCell
+                    component="th"
+                    scope="row"
+                    align="center"
+                    sx={{ border: "none" }}
+                  >
+                    {row.name}
+                  </TableCell>
+                  <TableCell
+                    component="th"
+                    scope="row"
+                    align="center"
+                    sx={{ border: "none" }}
+                  >
+                    {row.email}
+                  </TableCell>
+                  <TableCell
+                    component="th"
+                    scope="row"
+                    align="center"
+                    sx={{ border: "none" }}
+                  >
+                    {row.role}
+                  </TableCell>
+                  <TableCell
+                    component="th"
+                    scope="row"
+                    align="center"
+                    sx={{ border: "none" }}
+                  >
+                    Action items
+                  </TableCell>
+                </TableRow>
+              ))}
             </TableBody>
           </Table>
         </TableContainer>
--- src/components/DataTable/DataTable.js
+++ src/components/DataTable/DataTable.js
@@ -2,20 +2,7 @@ import { Checkbox, TableCell, TableRow } from "@mui/material";
 import React from "react";
 
 const DataTable = ({ dataList }) => {
-  return (
-    <div className="table-data">
-      {dataList.map((row) => (
-        <TableRow>
-          <TableCell>
-            <Checkbox />
-          </TableCell>
-          <TableCell>{row.name}</TableCell>
-          <TableCell>{row.email}</TableCell>
-          <TableCell>{row.role}</TableCell>
-        </TableRow>
-      ))}
-    </div>
-  );
+  return <div className="table-data"></div>;
 };
 
 export default DataTable;
